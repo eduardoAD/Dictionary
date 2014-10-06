a66ee07cd0e6893fa956749f83f46f7424be4936
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-06 01:33:39 -0500

    initial setup

--- Dictionary/main.m
+++ Dictionary/main.m
@@ -6,12 +6,24 @@
 //  Copyright (c) 2014 Globant. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+int main() {
+    //NSDictionary *book = [[NSDictionary alloc] initWithObjectsAndKeys:@"Code Da Vinci",@"title",@"Dan Drown",@"author", nil];
+    NSDictionary *book = @{@"title":@"Da Vinci's Code",@"author":@"Dan Drown"};
+
+    NSLog(@"book has %@", book);
+
+    NSString *string = [book objectForKey:@"author"];
+    NSLog(@"string %@", string);
+
+    NSMutableDictionary *mutableBook = [NSMutableDictionary dictionaryWithDictionary:book];
+    [mutableBook setObject:@2000 forKey:@"year"];
+    NSLog(@"%@", mutableBook);
+
+    for (NSString *string in [mutableBook allKeys]) {
+        NSLog(@"key %@ value %@", string, [mutableBook objectForKey:string]);
     }
+
     return 0;
 }
